36862554e7f1262cce9d4bfe1da87dab6a96aed6
patrick ntwali <patrick_ntwari@yahoo.com>
2022-11-29 00:29:13 +0200

    PNT: Completed Onion Arch module

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -1,5 +1,8 @@
-﻿using Nptk.Learning.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using Nptk.Learning.Contracts;
 using Nptk.Learning.LoggerSerice;
+using Nptk.Learning.Repository;
+using Nptk.Learning.Service.Contracts;
 
 namespace Nptk.Learning.Api.Extensions
 {
@@ -24,8 +27,20 @@ namespace Nptk.Learning.Api.Extensions
 
                 }
                 );
+
         public static void ConfigureLoggerService(this IServiceCollection services) =>
         services.AddSingleton<ILoggerManager, LoggerManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services) =>
+        services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        public static void ConfigureServiceManager(this IServiceCollection services) =>
+        services.AddScoped<IServiceManager, Service.ServiceManager>();
+
 
 
     }
