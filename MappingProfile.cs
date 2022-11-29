1f1cfba534c3fe2fe8553ca7249aa029c64b1d63
patrick ntwali <patrick_ntwari@yahoo.com>
2022-11-29 12:01:16 +0200

    PNT: completed handling http gets and getting additional resources in an http request

---
+++ MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Nptk.Learning.Entities;
+using Nptk.Learning.Shared.DataTransferObjects;
+
+namespace Nptk.Learning.Main
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Company, CompanyDto>()
+            .ForCtorParam("FullAddress",
+            opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+
+            CreateMap<Employee, EmployeeDto>();
+
+        }
+    }
+}
